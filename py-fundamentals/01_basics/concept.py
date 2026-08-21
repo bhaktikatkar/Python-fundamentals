eb57670752d7982b8a2425fe456282d644aa9fde
Bhakti Katkar <katkarbhakti684@gmail.com>
2026-08-21 20:21:44 +0530

    Add python basics - Day1

---
+++ py-fundamentals/01_basics/concept.py
@@ -0,0 +1,45 @@
+# Python Fundamentals-Day 1
+# Topic: Python Basics
+
+# Printing
+print("Hello, Python!")
+
+# Variables
+name = "Bhakti"
+age = 22
+height = 5.4
+
+print(name)
+print(age)
+print(height)
+
+# Checking data types
+print(type(name))
+print(type(age))
+print(type(height))
+
+
+# -----------------------------------------------------------------------------
+# Day 1 Practice
+# -----------------------------------------------------------------------------
+
+# 1. Storing college,year and course in variables
+college = "JNEC"
+year = 4
+course = "Robotics and AI"
+
+print(college)
+print(year)
+print(course)
+
+
+# 2. Check the data types
+x = 100
+y = 10.5
+z = "Python"
+
+print(type(college))
+print(type(year))
+print(type(course))
+
+
---
+++ py-fundamentals/01_basics/practice.py
@@ -0,0 +1,21 @@
+# Day 1 Practice
+
+# 1. Storing college, year and course in variables
+college = "JNEC"
+year = 4
+course = "Robotics and AI"
+
+print(college)
+print(year)
+print(course)
+
+
+# 2. Check the data types
+x = 100
+y = 10.5
+z = "Python"
+
+print(type(college))
+print(type(year))
+print(type(course))
+
